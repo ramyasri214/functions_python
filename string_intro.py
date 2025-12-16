583ec060c7905a8b44e54c4cf9bd4a57a8ea7a0f
ramyasri214 <ramyasrikethana@gmail.com>
2025-12-16 15:05:34 +0530

    Add files via upload

---
+++ string_intro.py
@@ -0,0 +1,29 @@
+# 🔤 What is a String?
+
+# A string is a collection of characters
+
+# Written inside single quotes ' ' or double quotes " "
+
+# Each character has an index (starts from 0)
+
+# Strings are immutable (cannot be changed)
+
+# Example:
+
+name = "string"
+# index: 0 1 2 3 4 5
+
+
+
+#  1. How to create a string
+#  Literal way
+a = "10"
+b = 'hello'
+
+# 2️ Constructor way
+h = str("hello")
+
+# ✅ Accessing characters of a string
+name = "string"
+print(name[0])   # s
+print(name[3])   # i
